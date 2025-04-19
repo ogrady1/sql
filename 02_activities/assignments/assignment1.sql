b467b667cdba836add4cd987cd8a72734a753462
Ryan O'Grady <ogrady.works@gmail.com>
2025-04-19 00:36:16 -0400

    updated section 2, completed section 3 except date

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -6,14 +6,12 @@
 /* 1. Write a query that returns everything in the customer table. */
 SELECT * FROM customer;
 
-
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
 SELECT *
 FROM customer
 ORDER BY customer_last_name, customer_first_name
 LIMIT 10;
-
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
@@ -39,14 +37,11 @@ SELECT *,
 FROM customer_purchases
 WHERE vendor_id >= 8 AND vendor_id <= 10;
 
-
-
 -- option 2
 SELECT *,
        quantity * cost_to_customer_per_qty AS price
 FROM customer_purchases
 WHERE vendor_id  BETWEEN 8 AND 10;
-
 
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
@@ -82,27 +77,39 @@ FROM product;
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
-SELECT v*, vba.*
-FROM vendor v
-INNER JOIN vendor_booth_assignments vba
-    ON v.vendor_id = vba.vendor_id
-ORDER BY v.vendor_name, vba.market_date;
+
+SELECT *
+FROM vendor
+INNER JOIN vendor_booth_assignments 
+USING (vendor_id)
+ORDER BY vendor_name, market_date;
 
 /* SECTION 3 */
 
 -- AGGREGATE
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
-
-
+SELECT 
+    vendor_id, 
+    COUNT(*) AS times_rented
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
-
-
+SELECT 
+	customer_first_name,
+	customer_last_name,
+	SUM (quantity * cost_to_customer_per_qty) as purchases
+FROM customer
+INNER JOIN customer_purchases
+USING (customer_id)
+GROUP BY customer_id
+HAVING purchases > 2000
+ORDER BY customer_last_name, customer_first_name;
 
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
@@ -116,6 +123,13 @@ When inserting the new vendor, you need to appropriately align the columns to be
 VALUES(col1,col2,col3,col4,col5) 
 */
 
+
+CREATE TEMPORARY TABLE IF NOT EXISTS temp.new_vendor AS
+SELECT *
+FROM vendor;
+
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
 
 
 -- Date
